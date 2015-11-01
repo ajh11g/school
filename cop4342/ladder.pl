e24497519c16feb0a5818843145a6222c14b4939
Aaron Hoffmann <ajh11g@my.fsu.edu>
2015-11-01 15:47:45 -0500

    Word ladder game

--- cop4342/ladder.pl
+++ cop4342/ladder.pl
@@ -7,15 +7,28 @@ my $startWord = $ARGV[0];
 my $endWord = $ARGV[1];
 my $wordFile = "wordlist.txt";
 my @arrayLetters = ("a".."z");
+my $line = 0;
 
 # open the word list
 open (IN, $wordFile) or die "Cannot open file: $!\n";
-
-print "Starting word: $startWord\n";
-print "Ending word: $endWord\n";
+checkWord();
 
 # start word ladder
 
+# if starting word or end word does not appear in wordlist
+sub checkWord {
+	while (<IN>) {
+		if (m/^$startWord$/i) {
+			print "'$startWord' has been found in the word list.\n";
+		}
+		if (m/^$endWord$/i) {
+			print "'$endWord' has been found in the word list.\n";
+		}
+	}
+}
+
+# else change on letter in starting word until match in wordlist
+# repeat until end word
 
 # close word list
 close (IN);
