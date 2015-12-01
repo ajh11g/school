790a159e251df531caecdc83829dcae56b78c5e4
Aaron Hoffmann <ajh11g@my.fsu.edu>
2015-11-30 20:50:29 -0500

    Updates...

--- cop4342/ladder.pl
+++ cop4342/ladder.pl
@@ -1,6 +1,6 @@
 #!/usr/bin/perl -w
 # By Aaron Hoffmann
-# For COP4342
+# For COP4342 ***Unable to fully complete assignment***
 use strict;
 
 my $startWord = $ARGV[0];
@@ -12,23 +12,32 @@ my $line = 0;
 # open the word list
 open (IN, $wordFile) or die "Cannot open file: $!\n";
 checkWord();
+ladder();
 
-# start word ladder
-
-# if starting word or end word does not appear in wordlist
+# check to see if starting word or end word appears in wordlist
 sub checkWord {
 	while (<IN>) {
-		if (m/^$startWord$/i) {
-			print "'$startWord' has been found in the word list.\n";
+		if (m/^$startWord$/i) {		
+			print "'$startWord' has been found.\n";
 		}
 		if (m/^$endWord$/i) {
-			print "'$endWord' has been found in the word list.\n";
+			print "'$endWord' has been found.\n";
 		}
 	}
 }
 
-# else change on letter in starting word until match in wordlist
-# repeat until end word
+# word ladder start
+# brute force method of checking each combination of letter in the starting word
+# until match with a word on wordlist, move on to next possible combo
+# until the ending word is reached
+
+sub ladder {
+	foreach my $letter (@arrayLetters) {
+		$startWord = substr($startWord, 0, 1, "$letter");
+	}
+	print "$startWord";
+}
+
 
 # close word list
 close (IN);
